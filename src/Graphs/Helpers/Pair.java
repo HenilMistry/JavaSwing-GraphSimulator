4d552624525bd4c1373fc17c878e86caec790c00
DESKTOP-M4QI8VT\HENIL <henilmistry74496@gmail.com>
2022-10-25 18:57:31 +0530

    added all the helprs requited till now inside this package

--- src/Graphs/Helpers/Pair.java
+++ src/Graphs/Helpers/Pair.java
@@ -1,2 +1,27 @@
-package Graphs.Helpers;public class Pair {
+package Graphs.Helpers;
+
+import java.util.Comparator;
+
+public class Pair implements Comparator<Pair> {
+
+    public int u,v;
+
+    public Pair() {
+
+    }
+
+    public Pair(int u, int v) {
+        this.u = u;
+        this.v = v;
+    }
+
+    /**
+     * <p>
+     *    This will return the difference between second point {@code v}.
+     * </p>
+     * */
+    @Override
+    public int compare(Pair o1, Pair o2) {
+        return o1.v - o2.v;
+    }
 }
